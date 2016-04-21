b3753b23591b61cd70d3f07551568105ceae18f8
Justin <japavoncello@gmail.com>
2016-04-21 03:46:26 -0400

    no message

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -12,6 +12,24 @@ ATank::ATank()
 
 }
 
+void ATank::SetTurretChildActor(UChildActorComponent* TurretFromBP)
+{
+	if (!TurretFromBP) { return; }
+	Turret = TurretFromBP;
+}
+
+void ATank::SetBarrelChildActor(UChildActorComponent* BarrelFromBP)
+{
+	if (!BarrelFromBP) { return; }
+	Barrel = BarrelFromBP;
+}
+
+void ATank::SetTankChildActor(UChildActorComponent* TankFromBP)
+{
+	if (!TankFromBP) { return; }
+	Tank = TankFromBP;
+}
+
 // Called when the game starts or when spawned
 void ATank::BeginPlay()
 {
@@ -23,18 +41,43 @@ void ATank::BeginPlay()
 void ATank::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-
+	GetWorld()->DeltaTimeSeconds;
 }
 
 // Called to bind functionality to input
 void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 {
 	Super::SetupPlayerInputComponent(InputComponent);
-
-	InputComponent->BindAction("Turret_Clockwise", IE_Pressed, this, &ATank::RotateTurret);
+	InputComponent->BindAxis("Rotate_Turret", this, &ATank::RotateTurret);
+	InputComponent->BindAxis("Elevate_Turret", this, &ATank::ElevateTurret);
+	InputComponent->BindAxis("Move_Tank", this, &ATank::MoveTank);
+	InputComponent->BindAxis("Rotate_Tank", this, &ATank::RotateTank);
 }
 
-void ATank::RotateTurret()
+void ATank::RotateTurret(float Speed)
 {
-	
+	if (!Turret) { return; }
+	float Rotation = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+	Turret->AddRelativeRotation(FRotator(0, Rotation, 0));
+}
+
+void ATank::ElevateTurret(float Speed)
+{
+	if (!Barrel) { return; }
+	float Rotation = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+	Barrel->AddRelativeRotation(FRotator(-Rotation, 0, 0));
+}
+
+void ATank::MoveTank(float Speed)
+{
+	if (!Tank) { return; }
+	float Distance = GetWorld()->DeltaTimeSeconds * Speed * MovementSpeed;
+	Tank->AddRelativeLocation(Tank->GetForwardVector() * Distance);
+}
+
+void ATank::RotateTank(float Speed)
+{
+	if (!Tank) { return; }
+	float Rotation = GetWorld()->DeltaTimeSeconds * Speed * RotationSpeed;
+	Tank->AddRelativeRotation(FRotator(0, Rotation, 0));
 }
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -16,13 +16,35 @@ public:
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	virtual void Tick(float DeltaSeconds) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	void RotateTurret();
-	
+	UFUNCTION(BlueprintCallable, Category = Tank)
+	void SetTurretChildActor(UChildActorComponent* TurretFromBP);
+
+	UFUNCTION(BlueprintCallable, Category = Tank)
+		void SetBarrelChildActor(UChildActorComponent* BarrelFromBP);
+
+	UFUNCTION(BlueprintCallable, Category = Tank)
+		void SetTankChildActor(UChildActorComponent* TankFromBP);
+
+private:
+	UChildActorComponent* Turret = nullptr;
+	UChildActorComponent* Barrel = nullptr;
+	UChildActorComponent* Tank = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		float RotationSpeed = 120;
+
+	UPROPERTY(EditAnywhere)
+		float MovementSpeed = 120;
+
+	void RotateTurret(float Speed);
+	void ElevateTurret(float Speed);
+	void MoveTank(float Speed);
+	void RotateTank(float Speed);
 };
